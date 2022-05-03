f12d195e22055b2933ec2cfdc6288cd9dc0fb40b
Akanksha <b.akanksha6@gmail.com>
2022-05-03 17:00:23 +0530

    home component modified

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./card.css";
 
-const Card = ({img, title, price, originalPrice, discount}) => {
+const Card = ({ img, title, price, originalPrice, discount, rating }) => {
   return (
     <div className="card">
-      <img
-        src={img}
-        alt="phone image"
-        className="card__image"
-      />
-      <h4 className="card__title">
-        {title}
-      </h4>
+      <img src={img} alt="product" className="card__image" />
+      <h4 className="card__title">{title}</h4>
+      <div className="card__rating">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <p key={i}>⭐</p>
+          ))}
+      </div>
       <p className="card__price">
-        ${price} <small className="card__priceStrike">${originalPrice}</small> <small className="card__discount">{discount}% OFF</small>
+        ${price} <small className="card__priceStrike">${originalPrice}</small>{" "}
+        <small className="card__discount">{discount}% OFF</small>
       </p>
       <button className="card__cartbutton">Add to cart</button>
     </div>
